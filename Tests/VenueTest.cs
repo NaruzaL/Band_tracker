d18b8554261742a5f36301c07b1dab6da25e743a
Pete Lazuran <pdlazuran@gmail.com>
2017-06-16 10:02:37 -0700

    add getBands and addBand method to Venue class, passing all 12 tests

--- Tests/VenueTest.cs
+++ Tests/VenueTest.cs
@@ -60,6 +60,44 @@ namespace BandTracker
       Assert.Equal(testVenue, foundVenue);
     }
 
+    [Fact]
+    public void Test_AddBand_AddsBandToVenue()
+    {
+
+      Venue testVenue = new Venue("Roseland", new DateTime(2011, 1 , 1), 1);
+      testVenue.Save();
+
+      Band testBand = new Band("Sevendust", "Heavy Metal");
+      testBand.Save();
+      Band testBand2 = new Band("Them Crooked Vultures", "Hard Rock");
+      testBand2.Save();
+
+      testVenue.AddBand(testBand);
+      testVenue.AddBand(testBand2);
+      List<Band> result = testVenue.GetBands();
+      List<Band> testList = new List<Band>{testBand, testBand2};
+      Assert.Equal(testList, result);
+    }
+      //As Written these are the same thing with different test names
+    [Fact]
+    public void Test_GetBand_RetrievesAllBandWithVenue()
+    {
+      Venue testVenue = new Venue("Rose Gardenk", new DateTime(2008, 12, 4));
+      testVenue.Save();
+
+      Band firstBand = new Band("Evercleaer", "Rock");
+      firstBand.Save();
+      Band secondBand = new Band("Queens of the Stone Age", "Rock");
+      secondBand.Save();
+
+      testVenue.AddBand(firstBand);
+      testVenue.AddBand(secondBand);
+
+      List<Band> testBandList = new List<Band> {firstBand, secondBand};
+      List<Band> resultBandList = testVenue.GetBands();
+      Assert.Equal(testBandList, resultBandList);
+    }
+
     public void Dispose()
     {
       Venue.DeleteAll();
